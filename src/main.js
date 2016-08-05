4207cabe50701c893a6443460de370d68d7863f6
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-08-04 21:33:29 -0700

    intro

--- src/main.js
+++ src/main.js
@@ -6,3 +6,9 @@ new Vue({
   el: 'body',
   components: { App }
 })
+
+/**
+ * INSTRUCTOR NOTES - 2min
+  1. https://vuejs.org/guide/index.html then take them to the JSFiddle link on the page to go over the basics
+  2. Talk about your experience, how excited you are about getting started, where we came from and where we are going...etc
+ */
