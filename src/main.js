125c009d04c6362a739e071dcea01929180b4d4d
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-08-04 21:57:36 -0700

    2-Environment Setup

--- src/main.js
+++ src/main.js
@@ -8,7 +8,16 @@ new Vue({
 })
 
 /**
- * INSTRUCTOR NOTES - 2min
-  1. https://vuejs.org/guide/index.html then take them to the JSFiddle link on the page to go over the basics
-  2. Talk about your experience, how excited you are about getting started, where we came from and where we are going...etc
+ * INSTRUCTOR NOTES - 5min
+  1. Talk about frameworks available online that can quickly get you set up like vue-cli
+  2. Inform student that the following will be necessary and to not worry, resources for all explained will be in the resources doc
+    - Atom or Sublime, Chrome with vue-devtools, Sourcetree
+    - Babel transpiler/mod for text editor
+    - Have them install xCode if they are using a Mac and launch the Apple Simulator with iPhone 6
+    - Show them how to split the screen to have the editor and the terminal one one screen
+    - Node (tell them to pause and follow the instructions on how to get node set up)
+    - eslint -g, babel-core -g, webpack -g, vue-cli -g walk student through installing vue-devtools and Sourcetree while npm packages are loading
+    - vue-cli - search on web to pull up docs and walk student through (will take time, flip back to vue docs and render lists or pause recording)
+    - go into code repository and "vue init webpack vue-youtube-api"
+    - cd into project, open up the project and end session
  */
