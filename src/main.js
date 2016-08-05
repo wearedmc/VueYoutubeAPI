765b00f702d73e397508488679056a0c3fffc3fa
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-08-04 22:12:14 -0700

    3-starting-fresh

--- src/main.js
+++ src/main.js
@@ -9,15 +9,10 @@ new Vue({
 
 /**
  * INSTRUCTOR NOTES - 5min
-  1. Talk about frameworks available online that can quickly get you set up like vue-cli
-  2. Inform student that the following will be necessary and to not worry, resources for all explained will be in the resources doc
-    - Atom or Sublime, Chrome with vue-devtools, Sourcetree
-    - Babel transpiler/mod for text editor
-    - Have them install xCode if they are using a Mac and launch the Apple Simulator with iPhone 6
-    - Show them how to split the screen to have the editor and the terminal one one screen
-    - Node (tell them to pause and follow the instructions on how to get node set up)
-    - eslint -g, babel-core -g, webpack -g, vue-cli -g walk student through installing vue-devtools and Sourcetree while npm packages are loading
-    - vue-cli - search on web to pull up docs and walk student through (will take time, flip back to vue docs and render lists or pause recording)
-    - go into code repository and "vue init webpack vue-youtube-api"
-    - cd into project, open up the project and end session
+  1. Show them the app in the browser and talk a bit about webpack, walk them through the configuration and a little bit about the folder structure + tests (we will cover that more later)
+  2. Tell them it's your policy not to use any boilerplate code and delete everything inside the src folder
+  3. start with the main.js file and rebuild it, talking about the import statement and creating a new Vue instance like in jsFiddle
+  4. Create ./App.vue and copy what is there now (instructor copy) so they see what is the bare minimum and what most templates begin with
+  5. Pull up the balsamiq diagram and begin talking about the app we will being to construct (bulk of conversation)
+  6. Highlight the major components in the diagram and use that when you create your components inside of ./components/ (dont create them yet, end session)
  */
